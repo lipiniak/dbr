82937620591f353d4bb50542de8a4856a3977ccf
Bartłomiej Wiśniewski <bwisniewski@MacBook-Pro-Bartomiej.local>
2022-09-08 11:05:47 +0200

    initial commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    
+                    <div class="p-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta mi augue, at semper nisl venenatis vitae. Vivamus dictum lorem non ex rutrum, vitae condimentum odio volutpat. Mauris placerat erat sit amet odio pulvinar laoreet. Cras malesuada facilisis fermentum. Pellentesque tincidunt malesuada dolor, eu mattis mi vestibulum at. Etiam in ante sed nisl feugiat malesuada. Duis massa elit, tincidunt ut ultrices sit amet, tristique non leo. In euismod ullamcorper sapien, tincidunt aliquam sapien pellentesque sit amet. Nullam at scelerisque eros. Curabitur eu elit quis nisi pharetra pretium ac vitae orci. Aenean sodales tortor nec mauris feugiat dapibus. Sed at arcu velit. Cras magna leo, tempor a maximus sed, fringilla nec dolor.
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
---
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -0,0 +1,101 @@
+<script setup>
+import { ref } from 'vue';
+import { useForm } from '@inertiajs/inertia-vue3';
+import ActionSection from '@/Components/ActionSection.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import DialogModal from '@/Components/DialogModal.vue';
+import InputError from '@/Components/InputError.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+const confirmingUserDeletion = ref(false);
+const passwordInput = ref(null);
+
+const form = useForm({
+    password: '',
+});
+
+const confirmUserDeletion = () => {
+    confirmingUserDeletion.value = true;
+
+    setTimeout(() => passwordInput.value.focus(), 250);
+};
+
+const deleteUser = () => {
+    form.delete(route('current-user.destroy'), {
+        preserveScroll: true,
+        onSuccess: () => closeModal(),
+        onError: () => passwordInput.value.focus(),
+        onFinish: () => form.reset(),
+    });
+};
+
+const closeModal = () => {
+    confirmingUserDeletion.value = false;
+
+    form.reset();
+};
+</script>
+
+<template>
+    <ActionSection>
+        <template #title>
+            Usuń konto
+        </template>
+
+        <template #description>
+            Trwale usuń swoje konto.
+        </template>
+
+        <template #content>
+            <div class="max-w-xl text-sm text-gray-600">
+                Usunięcie konta skutkuje usunięciami wszystkich danych.
+            </div>
+
+            <div class="mt-5">
+                <DangerButton @click="confirmUserDeletion">
+                    Usuń konto
+                </DangerButton>
+            </div>
+
+            <!-- Delete Account Confirmation Modal -->
+            <DialogModal :show="confirmingUserDeletion" @close="closeModal">
+                <template #title>
+                    Usuń konto
+                </template>
+
+                <template #content>
+                    Czy na pewno chcesz usunąć konto?
+
+                    <div class="mt-4">
+                        <TextInput
+                            ref="passwordInput"
+                            v-model="form.password"
+                            type="password"
+                            class="mt-1 block w-3/4"
+                            placeholder="Hasło"
+                            @keyup.enter="deleteUser"
+                        />
+
+                        <InputError :message="form.errors.password" class="mt-2" />
+                    </div>
+                </template>
+
+                <template #footer>
+                    <SecondaryButton @click="closeModal">
+                        Anuluj
+                    </SecondaryButton>
+
+                    <DangerButton
+                        class="ml-3"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                        @click="deleteUser"
+                    >
+                        Usuń konto
+                    </DangerButton>
+                </template>
+            </DialogModal>
+        </template>
+    </ActionSection>
+</template>
